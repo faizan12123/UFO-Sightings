ca56d44732db4d1ef824faf1004a63c48398085a
Faizan <faizan121234@gmail.com>
2023-08-16 12:12:59 -0700

    updated the route, controller, and service for the /getUFOdata/all endpoint. Endpoint is successfully returnining all information in the database

--- src/controllers/getUFOall.js
+++ src/controllers/getUFOall.js
@@ -1,10 +1,12 @@
 const queryUFOall = require("../services/queryUFOall.js")
 
-const getUFOall = (req, res) => {
-    const UFOall = queryUFOall()
-  
-    // DO SOMETHING WITH THE USER LIST OR JUST RETURN IT
-    return res.json(UFOall)
-  };
-  
-module.exports = getUFOall
+const getUFOall = async (req, res) => {
+  try {
+    const UFOall = await queryUFOall();
+    return res.json(UFOall);
+  } catch (error) {
+    return res.status(500).json({ error: "Error fetching UFO data" });
+  }
+};
+
+module.exports = getUFOall;
