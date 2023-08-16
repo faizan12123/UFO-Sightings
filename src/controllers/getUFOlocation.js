84e15b1f79d49384258ee046b1a07fc24a0ea778
Faizan <faizan121234@gmail.com>
2023-08-16 15:07:50 -0700

    updated the controller and services for the getUFOlocation.js files. This endpoint will return UFO data from the database based off of the params: city, state, and country. The scraper was also updated to fill in gaps for the images column

--- src/controllers/getUFOlocation.js
+++ src/controllers/getUFOlocation.js
@@ -1,10 +1,35 @@
 const queryUFOlocation = require("../services/queryUFOlocation.js")
 
-const getUFOlocation = (req, res) => {
-    const UFOlocation = queryUFOlocation()
-  
-    // DO SOMETHING WITH THE USER LIST OR JUST RETURN IT
-    return res.json(UFOlocation)
-  };
-  
-module.exports = getUFOlocation
+const getUFOlocation = async (req, res) => {
+  const { state, city, country } = req.query;
+
+  // Create an object to store only the existing parameters
+  const location = {};
+
+  try {
+    if (state) {
+      location.state = decodeURIComponent(state).split(',');
+    }
+    if (city) {
+      location.city = decodeURIComponent(city).split(',');
+    }
+    if (country) {
+      location.country = decodeURIComponent(country).split(',');
+    }
+
+    const UFOlocation = await queryUFOlocation(location);
+    
+    //if there is no results from the query, respond with a message to the user
+    if(UFOlocation.length > 0) {
+      return res.json(UFOlocation);
+    } else{
+      return res.send("No results matching this search")
+    }
+    
+  } catch (error) {
+    console.error("Error decoding URI:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = getUFOlocation;
--- src/services/queryUFOlocation.js
+++ src/services/queryUFOlocation.js
@@ -1,6 +1,27 @@
-const queryUFOlocation = (id) => {
-    //talk to database
-    return
-  }
+const db = require('../models/db.js');
 
-  module.exports = queryUFOlocation
+const queryUFOlocation = async (location) => {
+  const { city, state, country } = location;
+  try {
+    let queryBuilder = db.select().table('ufo');
+
+    if (city) {
+      queryBuilder.whereIn('city', city);
+    } 
+    if (state) {
+      queryBuilder.whereIn('state', state);
+    } 
+    if (country) {
+      queryBuilder.whereIn('country', country);
+    }
+
+    const data = await queryBuilder;
+    
+    return data;
+  } catch (error) {
+    console.error('Error querying UFO data:', error);
+    throw error;
+  }
+};
+
+module.exports = queryUFOlocation;
