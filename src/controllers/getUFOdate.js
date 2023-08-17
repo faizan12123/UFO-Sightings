6917342882fc44c46c5cf95f1d795da30bcc8f4d
Faizan <faizan121234@gmail.com>
2023-08-16 17:31:35 -0700

    updating the controller and service file for the getUFOdate and creating an endpoint that allows people to filter the database data by the date of occurence. I also changed the scraper.js file to now seed database with just the date instead of the date and the time for the date of occurrence

--- src/controllers/getUFOdate.js
+++ src/controllers/getUFOdate.js
@@ -1,10 +1,31 @@
 const queryUFOdate = require("../services/queryUFOdate.js")
 
-const getUFOdate = (req, res) => {
-    const UFOdate = queryUFOdate()
+const getUFOdate = async (req, res) => {
+
+  let {dateOfOccurrence} = req.query
+  try {
+    if (dateOfOccurrence) {
+      dateOfOccurrence = decodeURIComponent(dateOfOccurrence)
+      dateOfOccurrence = dateOfOccurrence.split(',');
   
-    // DO SOMETHING WITH THE USER LIST OR JUST RETURN IT
-    return res.json(UFOdate)
-  };
+
+      for (let date of dateOfOccurrence) {
+        if(!date.match(/^\d{1,2}\/\d{1,2}\/\d{2}$/)){
+          return res.status(400).json({ error: "Invalid date format." });
+        }
+      }
+    }
+
+    const UFOdate = await queryUFOdate(dateOfOccurrence)
+    if(UFOdate.length > 0) {
+      return res.json(UFOdate);
+    } else{
+      return res.status(400).json({ error: "No results matching this search" })
+    }
+} catch (error) {
+  console.error("Error decoding URI:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
   
 module.exports = getUFOdate
--- src/services/queryUFOdate.js
+++ src/services/queryUFOdate.js
@@ -1,6 +1,21 @@
-const queryUFOdate = (id) => {
-    //talk to database
-    return
+const db = require('../models/db.js');
+
+const queryUFOdate = async (dateOfOccurrence) => {
+  try {
+    let queryBuilder = db.select().table('ufo');
+    
+    if (dateOfOccurrence) {
+      queryBuilder.whereIn('incident_date', dateOfOccurrence);
+    } 
+
+    const data = await queryBuilder;
+    
+    return data;
+    
+  } catch(error) {
+    console.error('Error querying UFO data:', error);
+    throw error;
+  }
   }
 
   module.exports = queryUFOdate
