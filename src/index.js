d59f27203fb1f45c3958c794d22d0c7233b99dd6
Faizan <faizan121234@gmail.com>
2023-08-16 23:42:41 -0700

    added API rate limiting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-// const { dataScraper } = require('./scraper/scraper.js')
 
 // Make express server
 const express = require("express")
@@ -7,6 +6,13 @@ const app = express()
 const port = process.env.PORT || 3001
 
 const getUFOdataRoutes = require("./routes/getUFOdata.js")
+
+const limiter = require('./middlewares/rateLimiter.js');
+
+// Apply rate limiter middleware to application
+app.use(limiter)
+
+// API route for getting UFO data
 app.use("/getUFOdata", getUFOdataRoutes)
 
 // Run the scrapeAndSeed script immediately
