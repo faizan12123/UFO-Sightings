9c603308ced107d7836a34fabaaa4d53ba5dbd13
Faizan <faizan121234@gmail.com>
2023-08-23 15:12:38 -0700

    added UI frontend for the application. Temporarily allowed CORS to allow cross origin requests from any frontends

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,11 +1,27 @@
 const limiter = require('../middlewares/rateLimiter.js');
 const express = require("express")
 const getUFOdataRoutes = require("../routes/getUFOdata.js")
+const cors = require('cors');
 
 const server = () => {
     const app = express()
+
+    const allowedOrigins = ['http://127.0.0.1:5500/']; // Replace with your domain
+    const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+        } else {
+        callback(new Error('Not allowed by CORS'));
+        }
+    }
+    };
+
     // Apply rate limiter middleware to application
     app.use(limiter)
+
+    // Apply CORS middleware using corsOptions
+    app.use(cors());
     
     // API route for getting UFO data
     app.use("/getUFOdata", getUFOdataRoutes)
